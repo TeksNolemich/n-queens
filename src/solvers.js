020aa9ce29c41598158beda46d59228076c00562
arthurtharp <atharp@uci.edu>
2018-08-08 20:28:06 -0700

    rooks tests passing

--- src/solvers.js
+++ src/solvers.js
@@ -16,16 +16,38 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({'n': n}); 
 
+  // console.dir(solution.__proto__.togglePiece);
+  for (var i = 0; i < n; i++) {
+    solution.togglePiece(i,i);
+  }
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
+  var solutionCount = 0; //fixme
+  var board = new Board({'n': n});
+  var placeRooks = function(row) {
+  //var placeRooks = function(row,column) {
+    if(n === row) {
+      solutionCount++;
+      return;
+      //return 
+    } else {
+      for (var col = 0; col < n; col++) {
+        board.togglePiece(row,col);
+        if(!board.hasColConflictAt(col)) {
+        // if(!board.hasAnyRooksConflicts()) {
+          placeRooks(row+1);
+        }
+        board.togglePiece(row,col);
+      }
+    }  
+  } 
+  placeRooks(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
